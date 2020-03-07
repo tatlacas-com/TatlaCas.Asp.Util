f4cbf34029fc25e035b9f4a58e026693aef46fb2
Tatenda Caston Hove <tathove@tatlacas.com>
2020-03-07 12:55:48 +0200

    Added embedded Attributes

---
+++ HtmlForms/HasRightEmbeddedAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TatlaCas.Asp.Utils.HtmlForms
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class HasRightEmbeddedAttribute : Attribute
+    {
+    }
+}
