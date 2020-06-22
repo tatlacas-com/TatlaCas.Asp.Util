05c8a8b738c4acb9bc2121c32ea27057001b303a
Tatenda <tathove@gmail.com>
2020-06-23 00:27:44 +0200

    Update DataTableQuery.cs
    Include QueryObject

--- ViewModels/DataTableQuery.cs
+++ ViewModels/DataTableQuery.cs
@@ -1,9 +1,12 @@
 
+using System.Text.Json;
+
 namespace TatlaCas.Asp.Core.Util.ViewModels
 {
     public class DataTableQuery
     {
        public string GeneralSearch { get; set; }
         public int QueryId { get; set; }
+        public JsonElement QueryObject { get; set; }
     }
 }
