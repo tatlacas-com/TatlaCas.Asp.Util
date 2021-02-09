638ddb75006e32576172bc8b514aefa219569ce3
Tatenda <tathove@gmal.com>
2021-02-09 12:52:55 +0200

    Create NotificationParams.cs

---
+++ ViewModels/NotificationParams.cs
@@ -0,0 +1,12 @@
+﻿namespace TatlaCas.Asp.Core.Util.ViewModels
+{
+    public class NotificationParams
+    {
+        public string Param3 { get; set; }
+        public string Param1 { get; set; }
+        public string Param2 { get; set; }
+        public string Param4 { get; set; }
+        public string Param5 { get; set; }
+        public string Param6 { get; set; }
+    }
+}
