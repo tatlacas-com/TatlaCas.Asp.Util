8d1abdaac8be21bde1671595a429b8ac587f7e48
Tatenda <tathove@gmail.com>
2022-03-27 22:29:16 +0200

    Update IResource.cs

--- Resources/IResource.cs
+++ Resources/IResource.cs
@@ -4,9 +4,5 @@ namespace TatlaCas.Asp.Core.Util.Resources;
 
 public interface IResource
 {
-    /*public string ViewKey { get; set; }
-    public string UniqueKey { get; set; }
-    public string CellViewType { get; set; }
-    Dictionary<string, List<IResourceOption>> Options { get; set; }*/
     bool IsValid();
 }
